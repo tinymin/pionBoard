868e40bedaeaf73611326dfa884e6c3ea74b6c27
Suncheol Kim <tinymin@yahoo.co.kr>
2017-07-03 12:54:35 +0900

    SpringBoot 프로젝트 신규생성   - Lombok   - H2   - SpringMVC

---
+++ src/main/java/pionnet/PionboardApplication.java
@@ -0,0 +1,12 @@
+package pionnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PionboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PionboardApplication.class, args);
+	}
+}
