38c7690213099a52e5ea6e96c803a96c7e5ad913
SuncheolKim <tinymin84@gmail.com>
2017-07-03 22:53:59 +0900

    Member 클래스 제거, Article 클래스 추가

---
+++ src/main/java/pionnet/dto/Comment.java
@@ -0,0 +1,7 @@
+package pionnet.dto;
+
+/**
+ * Created by tinym on 2017-07-03.
+ */
+public class Comment {
+}
---
+++ src/test/java/pionnet/ArticleTest.java
@@ -0,0 +1,15 @@
+package pionnet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Kim.Suncheol on 2017-07-03.
+ */
+public class ArticleTest {
+    @Test
+    public void 글객체테스트() throws Exception {
+
+    }
+}
